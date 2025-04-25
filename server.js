0a16c3d5d64d35cee091224006626773e6c69126
RamDev <daudalaramadan2@gmail.com>
2025-04-25 10:09:42 +0000

    💫feat: add dotenv configuration and parsing capabilities
    - Introduced dotenv configuration via environment variables in config.js
    - Implemented CLI options parsing in cli-options.js
    - Added environment options handling in env-options.js
    - Created TypeScript 💫 Developed main functionality for loading and parsing .env files in main.js
    - Added decryption support for encrypted .env.vault files
    - Updated package.json with new version and dependencies

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 import express from "express";
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 const app = express();
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`💫Server is running on port ${PORT}`);
 });
 
 app.get("/", (req, res) => {
