2b994ea9ed769dc68774db4273cc8672432aae74
RamDev <daudalaramadan2@gmail.com>
2025-04-27 23:49:33 +0000

    done with getAllPosts

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,9 +1,14 @@
 import Posts from "../models/posts.js";
+import createHttpErrors from "http-errors";
 
 export async function getAllPosts(req, res) {
   try {
-    const Allposts = await Posts.find().populate({ path: "title" });
+    const Allposts = await Posts.find();
     console.log(Allposts);
+    if (!Allposts)
+      return next(createHttpErrors(404, "counldm=nt get all the posts "));
+
+    res.send(json(Allposts)).status(404);
   } catch (err) {
     console.log(err);
   }
