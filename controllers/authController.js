147deec64f2e03285d77265ac35d51999ac63308
RamDev <daudalaramadan2@gmail.com>
2025-05-07 03:06:40 +0000

    hashing the password

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,9 +1,11 @@
 import UserSchema from "../models/users.js";
+// this is used to harsh passwords ie bcrypt
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
-export function register(req, res, nest) {
+export async function register(req, res, nest) {
   res.send("helo");
   const { username, password, role } = req.body;
+  const hashedpassword = bcrypt.hash();
 }
 
 export function login(req, res, next) {
