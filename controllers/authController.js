fe6ffb80e1ef9c8eb8c30e677f9bd67f9342b370
RamDev <daudalaramadan2@gmail.com>
2025-05-13 02:45:38 +0000

    refactor: update register function to handle user registration and change login route to GET

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,9 +3,26 @@ import UserSchema from "../models/users.js";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 export async function register(req, res, nest) {
-  res.send("helo");
-  const { username, password, role } = req.body;
-  const hashedpassword = bcrypt.hash();
+  try {
+    //   res.send("helo");
+    const { username, password, role } = req.body;
+    //   the 10 added is a salt value that added to make sure similar passwords dont have the same hashed details
+    const hashedpassword = await bcrypt.hash(password, 10);
+    //   console.log(hashedpassword);
+
+    // 2. lets save the users details
+    const saveUser = new UserSchema({
+      username,
+      password: hashedpassword,
+      role,
+    });
+    await saveUser.save();
+    res
+      .status(201)
+      .json({ message: `User registered with the username ${username}` });
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 export function login(req, res, next) {
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,6 +3,6 @@ import express from "express";
 const router = express.Router();
 
 router.post("/register", register);
-router.post("/login", login);
+router.get("/login", login);
 
 export default router;
