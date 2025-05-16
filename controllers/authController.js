9057264e83b565122a73dcf73f9f909dcfe022fc
RamDev <daudalaramadan2@gmail.com>
2025-05-16 00:45:30 +0000

    fix: improve error handling in register function and implement login logic

--- controllers/authController.js
+++ controllers/authController.js
@@ -21,10 +21,25 @@ export async function register(req, res, nest) {
       .status(201)
       .json({ message: `User registered with the username ${username}` });
   } catch (error) {
-    console.log(error);
+    res.status(500).json({ message: `smt went wrong` });
   }
 }
 
-export function login(req, res, next) {
-  res.send("login");
+export async function login(req, res, next) {
+  try {
+    const { username, password } = req.body;
+    const user = await UserSchema.findone({ username });
+    if (!user) {
+      return res
+        .status(404)
+        .json({ message: `usename is  not found ${username}` });
+    }
+    // lets match the passwords
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: `invalid credentials ` });
+    }
+
+    const token = jwt({ id: user._id, role: user.role }, process.env.jwt);
+  } catch (error) {}
 }
